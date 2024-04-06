138631efd1f35e6b41a7661f0ce3530c7d38bcce
Gabriel5595 <gabriel.vcarvalho@al.infnet.edu.br>
2024-04-06 00:15:39 -0300

    Atualização da função de busca e imporessão de cliente por nome ou sobrenome

--- tests/request_client_search.py
+++ tests/request_client_search.py
@@ -8,18 +8,17 @@ from components.print_client_list import print_client_list
 
 def request_client_search(db_clients):
     while True:
-        
-        term = input("Please, enter the name or surname: ")
-        
         try:
             print("Would you like search by Name or Surname?")
             name_or_surname = int(input("1) Name\n2) Surname\n"))
             
             if name_or_surname == 1:
+                term = input("Please enter the name: ")
                 indexes = search_client(db_clients, term, True)
                 print_client_list(db_clients, indexes)
                 break
             elif name_or_surname == 2:
+                term = input("Please enter the surname: ")
                 indexes = search_client(db_clients, term, False)
                 print_client_list(db_clients, indexes)
                 break
